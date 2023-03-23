0088725eca1b0be5d8d10427b710f6530f8a26a7
Shujaat Siddiqui <shujaat.siddiqui@fitnessintl.com>
2023-03-23 15:10:17 -0700

    added reset and manual transaction logic

--- WebSocketSampleApp/src/app/device-manager/device-manager.component.ts
+++ WebSocketSampleApp/src/app/device-manager/device-manager.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FlowId } from 'src/Helper/FlowIdHelper';
-import { RequestDTO, RequestRoot, ResourceDTO } from 'src/models/RequestDTO';
+import { BeepRequestDTO, BeepResourceDTO, RequestDTO, RequestRoot, ResetResourceDTO, ResourceDTO, ManualTransactionResourceDTO } from 'src/models/RequestDTO';
 import { SettingsDTO } from 'src/models/SettingsDTO.model';
 import { WebsocketService } from 'src/Services/websocket.service';
 
@@ -12,28 +12,93 @@ import { WebsocketService } from 'src/Services/websocket.service';
 export class DeviceManagerComponent {
 
   settings: SettingsDTO = new SettingsDTO();
-  constructor(private websocketService: WebsocketService) { }
+  constructor(private websocketService: WebsocketService) {
+
+
+  }
 
   Beep() {
 
     var root = new RequestRoot();
-    var requestObj = new RequestDTO();
+    var requestObj = new BeepRequestDTO();
     requestObj.endpoint = "/upp/v1/device";
     requestObj.flow_id = FlowId.generate();
-    requestObj.resource = new ResourceDTO();
+    requestObj.resource = new BeepResourceDTO();
     requestObj.resource.duration = "click_length";
     requestObj.resource.tone = "low";
     requestObj.resource.type = "beep";
     root.request = requestObj;
 
     this.websocketService.receiveMessage().subscribe({
-      next: msg => console.log('message received: ' + JSON.stringify(msg)), // Called whenever there is a message from the server.
+      next: msg => alert('message received: ' + JSON.stringify(msg)), // Called whenever there is a message from the server.
       error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
       complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
     });
+
     this.websocketService.sendMessage(root);
   }
+
+  GetDirectoryListings() {
+    debugger;
+    var root = new RequestRoot();
+    var requestObj = new RequestDTO();
+    requestObj.flow_id = FlowId.generate();
+    requestObj.resource = new ResourceDTO();
+    root.request = requestObj;
+
+    this.websocketService.receiveMessage().subscribe({
+      next: msg => alert('message received: ' + JSON.stringify(msg)), // Called whenever there is a message from the server.
+      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    });
+
+    this.websocketService.sendMessage(root);
+  }
+
+  Reset()
+  {
+    //{"request":{"flow_id":"915323","resource":{"type":"reset","keep_form":false}}}
+
+    var root = new RequestRoot();
+    var requestObj = new RequestDTO();
+    requestObj.endpoint = "/upp/v1/device";
+    requestObj.flow_id = FlowId.generate();
+    requestObj.resource = new ResetResourceDTO();
+    root.request = requestObj;
+
+    this.websocketService.receiveMessage().subscribe({
+      next: msg => alert('Reset message received: ' + JSON.stringify(msg)), // Called whenever there is a message from the server.
+      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    });
+
+    this.websocketService.sendMessage(root);
+  }
+
+  Transaction()
+  {
+    //{"request":{"endpoint":"\/upp\/v1\/transaction","flow_id":"6442528","resource":{"type":"manual_entry","amount":"2501","fields":["pan","exp"]}}}
+    var root = new RequestRoot();
+    var requestObj = new RequestDTO();
+    requestObj.endpoint = "/upp/v1/transaction";
+    requestObj.flow_id = FlowId.generate();
+    var transactionRequestDTO = new ManualTransactionResourceDTO();
+    transactionRequestDTO.amount = 1000;
+    requestObj.resource = transactionRequestDTO;
+    root.request = requestObj;
+
+    this.websocketService.receiveMessage().subscribe({
+      next: msg => alert('Transaction message received: ' + JSON.stringify(msg)), // Called whenever there is a message from the server.
+      error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    });
+
+    this.websocketService.sendMessage(root);
+  }
+
 }
+
+
 
 
 
--- WebSocketSampleApp/src/models/RequestDTO.ts
+++ WebSocketSampleApp/src/models/RequestDTO.ts
@@ -1,25 +1,41 @@
 import { FlowId } from "src/Helper/FlowIdHelper";
 
-export class RequestRoot
-{
-  public request! : RequestDTO;
+export class RequestRoot {
+  constructor() {
+    this.request = new RequestDTO();
+  }
+  public request!: RequestDTO;
 }
 
 export class RequestDTO {
-  constructor() {
-    this.resource = new ResourceDTO();
-  }
   public flow_id: string = FlowId.generate();
+  public resource!: any
   public endpoint: string = ""
-  public resource!: ResourceDTO
 }
 
+export class BeepRequestDTO extends RequestDTO {
+}
+
+
 export class ResourceDTO {
+
+}
+
+export class BeepResourceDTO extends ResourceDTO {
   public type: string = ""
   public tone: string = ""
   public duration: string = ""
 }
 
-export class MessageEventDTO {
-  public type: string | undefined = "";
+export class ResetResourceDTO extends ResourceDTO {
+  public type: string = "reset"
+  public keep_form: boolean = false
 }
+
+export class ManualTransactionResourceDTO extends ResourceDTO {
+  public type: string = "manual_entry"
+  public amount : number = 0
+  public fields: string[] =  ["pan", "exp"];
+}
+
+
